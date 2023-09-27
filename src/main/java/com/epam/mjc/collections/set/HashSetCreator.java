760f999c923dfcbaaed37748151559c2c370b6fb
AbekM <asilbek.makhmudov@outlook.com>
2023-09-28 01:10:15 +0500

    donzo

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -4,6 +4,21 @@ import java.util.HashSet;
 import java.util.List;
 
 public class HashSetCreator {
-    public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+
+  public HashSet<Integer> createHashSet (List<Integer> sourceList) {
+    HashSet set = new HashSet<>();
+    for (Integer i : sourceList) {
+      if (i % 2 == 0) {
+        while (i % 2 == 0) {
+          set.add(i);
+          i /= 2;
+        }
+        set.add(i);
+      } else {
+        set.add(i);
+        set.add(i * 2);
+      }
     }
+    return set;
+  }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,9 +1,17 @@
 package com.epam.mjc.collections.set;
 
 import java.util.List;
+import java.util.NavigableSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        NavigableSet<Integer> tree = new TreeSet<>();
+        for (Integer i : sourceList) {
+            tree.add(i*i);
+        }
+        System.out.println(tree);
+        return tree.subSet(lowerBound, true, upperBound, true);
     }
 }
